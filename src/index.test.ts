9aa6f233a57bd740ad751bd851a1cc248d2c9cd0
Tomáš Mládek <t@mldk.cz>
2022-09-21 22:11:22 +0200

    chore: add instagram test, dotenv

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,7 @@
+import * as dotenv from 'dotenv';
 import { publishVideo } from '.';
 
+dotenv.config();
 jest.setTimeout(60 * 1000);
 
 describe('mock', () => {
@@ -19,4 +21,22 @@ describe('mock', () => {
     await expect(publishResult).resolves.not.toThrowError();
   });
 });
+
+
+describe('instagram', () => {
+  test('URL upload works', async () => {
+    const publishResult = publishVideo(
+      {
+        video: 'https://download.samplelib.com/mp4/sample-5s.mp4',
+        caption: 'Single video from Superface Upload Lib.',
+      },
+      'instagram',
+      {
+        parameters: {
+          accessToken: process.env['INSTAGRAM_ACCESS_TOKEN'],
+        },
+      }
+    );
+    await expect(publishResult).resolves.not.toThrowError();
+  });
 });
